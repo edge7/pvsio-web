beba231f20e4aefe8854380d6f73dbfbf7267671
Enrico D'Urso <e.durso@live.com>
2013-09-11 11:31:58 +0200

    Adding displayType.js

---
+++ public/pvsioweb/app/formal/pvs/prototypebuilder/displayType.js
@@ -0,0 +1,14 @@
+/**
+ * defines DisplayTypes
+ * @author Enrico D'Urso
+ * @date Sep 5, 2013 : 11:18:45 AM
+ */
+
+/*jslint vars: true, plusplus: true, devel: true, nomen: true, indent: 4, maxerr: 50, es5: true */
+/*global define, d3, require, __dirname, process*/
+define({
+	Classic:  "Classic",
+	Bbraun:   "Bbraun" ,
+	AlarisGP: "AlarisGP",
+	AlarisGH: "AlarisGH" 
+});
